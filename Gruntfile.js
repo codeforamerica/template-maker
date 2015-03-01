d8b5a248a2cf7f760f79d784535ad259b52bcfaa
bsmithgall <ben.smithgall@gmail.com>
2015-02-28 20:52:16 -0500

    added javascript base test scaffolding

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+
+    templateMaker: {
+      js: './template_maker/static/js',
+      lib: './template_maker/static/lib',
+      test: './template_maker/static/test'
+    },
+
+    karma: {
+      options: {
+        basePath: '.',
+        browsers: ['PhantomJS'],
+        frameworks: [
+          'mocha',
+          'chai',
+          'sinon'
+        ],
+        plugins: [
+          'karma-chai',
+          'karma-coverage',
+          'karma-mocha',
+          'karma-phantomjs-launcher',
+          'karma-sinon',
+          'karma-spec-reporter'
+        ],
+        files: [
+          // framework files
+          '<%= templateMaker.lib %>/angular/angular.js',
+          '<%= templateMaker.lib %>/angular-mocks/angular-mocks.js',
+          '<%= templateMaker.lib %>/angular-sanitize/angular-sanitize.js',
+          '<%= templateMaker.lib %>/angular-sanitize/angular-sanitize.js',
+          // app files
+          '<%= templateMaker.js %>/**/*.js',
+          // test files
+          '<%= templateMaker.test %>/unit/**/*.js',
+        ]
+      },
+      single: {
+        singleRun: true,
+        reporters: ['spec']
+      }
+    }
+
+  });
+
+  grunt.registerTask('test:unit', [
+    'karma:single'
+  ]);
+}
---
+++ template_maker/static/test/unit/builder/controllers/newBuilderCtrl.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  describe('Controllers', function() {
+
+    var expect = chai.expect;
+
+    // inject the builder module
+    beforeEach(module('builder'));
+
+    describe('newBuilderCtrl', function() {
+      var $scope, $rootScope, $controller, alert;
+      beforeEach(inject(function(_$rootScope_, _$controller_) {
+        $rootScope = _$rootScope_.$new()
+        $scope = $rootScope.$new();
+
+        var controller = _$controller_('newBuilderCtrl', {
+          $scope: $scope,
+          alert: sinon.spy()
+        });
+
+      }));
+
+      it('should trigger an alert on save', function() {
+        $rootScope.$broadcast('saveTemplate');
+        expect(alert).to.be.called;
+      })
+
+    });
+  });
+})();
