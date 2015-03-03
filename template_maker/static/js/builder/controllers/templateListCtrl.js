eb347271beb75ea7b9e58d6e275eecb4e0cffd8f
Ben Smithgall <ben.smithgall@gmail.com>
2015-03-03 01:04:34 -0500

    added js tests for new features

--- template_maker/static/js/builder/controllers/templateListCtrl.js
+++ template_maker/static/js/builder/controllers/templateListCtrl.js
@@ -1,14 +1,17 @@
 (function() {
   /*
-
+   While the majority of the functionality and layout for this view
+   is controlled by the backend/flask portion of the builder, the DELETE
+   method is handled here.
   */
   'use strict';
 
-  builder.controller('templateListCtrl', ['$scope', 'builderSubmit',
-    function($scope, builderSubmit) {
+  builder.controller('templateListCtrl', ['$scope', '$window', 'builderSubmit',
+    function($scope, $window, builderSubmit) {
 
       $scope.deleteTemplate = function(templateId) {
-        if(confirm('Are you sure you want to delete this template?')) {
+        var answer = $window.confirm('Are you sure you want to delete this template?');
+        if(answer) {
           builderSubmit.deleteTemplate(templateId).then(function(templateId) {
             var elem = angular.element('#template-' + templateId);
             elem.remove();
