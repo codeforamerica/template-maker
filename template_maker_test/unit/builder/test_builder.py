a17491ac08acdc4c71aeaed5793bfab64364164a
bsmithgall <ben.smithgall@gmail.com>
2015-03-12 01:28:19 -0400

    added new template test

--- template_maker_test/unit/builder/test_builder.py
+++ template_maker_test/unit/builder/test_builder.py
@@ -5,11 +5,14 @@ from template_maker.builder.models import (
     TemplateBase
 )
 
+def create_a_template():
+    now = datetime.datetime.utcnow()
+    return TemplateBase(created_at=now, updated_at=now, title='test', description='test')
+
 class TestBuilder(BaseTestCase):
     render_templates = False
     def test_list_view(self):
-        now = datetime.datetime.utcnow()
-        template = TemplateBase(created_at=now, updated_at=now, title='test', description='test')
+        template = create_a_template()
         db.session.add(template)
         db.session.commit()
 
@@ -18,3 +21,19 @@ class TestBuilder(BaseTestCase):
         response = self.client.get('/build/')
         assert response.status_code == 200
         self.assertEquals(len(self.get_context_variable('templates')), 1)
+
+
+    def test_new_template(self):
+        # it should get the proper template
+        response = self.client.get('/build/new')
+        assert response.status_code == 200
+        self.assert_template_used('builder/new.html')
+
+        post = self.client.post('/build/new', data=dict(
+            title='title',
+            description = 'description'
+        ))
+        # it should successfully add to the db and redirect us 
+        # to the right section page
+        assert post.status_code == 302
+        assert post.location == 'http://localhost/build/1/section/new'
