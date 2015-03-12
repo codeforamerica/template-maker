9245afc2a5f1fd25b1322772453b4d8b23a72bbb
Ben Smithgall <ben.smithgall@gmail.com>
2015-03-12 11:21:15 -0400

    basic froala editor handling

--- template_maker/static/js/froalaInit.js
+++ template_maker/static/js/froalaInit.js
@@ -1,14 +1,45 @@
 $(function() {
 
-  function getSelectionText() {
-    var text = '';
-    if (window.getSelection) {
-      text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-      text = document.selection.createRange().text;
-    }
-    return text;
+  var variableId = 1;
+
+  function createVariableHtml(modal) {
+    var _type = modal.find('.modal-variable-type').val();
+    var _name = modal.find('.modal-variable-name').val();
+
+    modal.find('.modal-variable-name').val('');
+
+    return '[[' + _type + ':' + _name + ']]';
   }
+
+  function createVariableShell(modal) {
+    var shell = '<span contenteditable=false class="fr-variable"' +
+    'id="variable-' + variableId + '">' + '[[]]</span>';
+    console.log(shell);
+    return shell;
+  }
+
+  function insertVariable() {
+    // open the modal
+    $('#myModal').modal('show');
+  }
+
+  // insert the shell on shown
+  $('#myModal').on('shown.bs.modal', function(e) {
+
+  });
+
+  // insert the actual variable on save
+  $('#myModal').find('.modal-add-variable').on('click', function(e) {
+    var modal = $('#myModal');
+    $('#variable-' + variableId).html(createVariableHtml(modal));
+    variableId++;
+  });
+
+  // remove the shell on close
+  $('#myModal').on('hide.bs.modal', function(e) {
+    $('#variable-' + variableId).remove();
+  });
+
 
   $('#widget')
     .html($('#sectionText').html())
@@ -28,15 +59,11 @@ $(function() {
           },
           callback: function () {
             var froala = this;
+            insertVariable()
 
-            $('#myModal').modal('show');
-            $('#myModal').on('hide.bs.modal', function(e) {
-              var modal = $(this);
-              froala.insertHTML('[[' + modal.find('.modal-variable-type').val() + ':' +
-                modal.find('.modal-variable-name').val() + ']]');
+            var shell = createVariableShell()
+            froala.insertHTML(shell);
 
-              froala.sync()
-            });
           }
         }
       }
