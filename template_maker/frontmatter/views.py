2b9d30673e307806998fa6611cde6620844192ca
bsmithgall <ben.smithgall@gmail.com>
2015-03-16 19:45:58 -0400

    added view at the root url

---
+++ template_maker/frontmatter/views.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+
+blueprint = Blueprint(
+    'frontmatter', __name__,
+    template_folder='../templates'
+)
+
+@blueprint.route('/')
+def index():
+    return render_template('frontmatter/index.html')
