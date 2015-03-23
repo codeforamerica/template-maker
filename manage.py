4cab0381205e036ee6ec39448ed532c219b79a16
bsmithgall <ben.smithgall@gmail.com>
2015-03-22 20:21:34 -0400

    added procfile, post-compile hooks for heroku deployment

--- manage.py
+++ manage.py
@@ -1,14 +1,26 @@
+import os
 from flask.ext.script import Manager, Server
 from flask.ext.migrate import MigrateCommand
+from flask.ext.assets import ManageAssets
 
 from template_maker.app import create_app
 from template_maker.settings import DevConfig, ProdConfig
 
-app = create_app(DevConfig)
+if os.environ.get('TEMPLATE_MAKER_ENV') == 'prod':
+    app = create_app(ProdConfig)    
+else:
+    app = create_app(DevConfig)
+
+def _make_context():
+    """Return context dict for a shell session so you can access
+    app and db by default.
+    """
+    return {'app': app, 'db': db}
 
 manager = Manager(app)
 manager.add_command('server', Server(port=9000))
 manager.add_command('db', MigrateCommand)
+manager.add_command('assets', ManageAssets)
 
 if __name__ == '__main__':
     manager.run()
--- template_maker/settings.py
+++ template_maker/settings.py
@@ -4,14 +4,14 @@ class Config(object):
     SECRET_KEY = os.environ.get('TEMPLATE_MAKER_SECRET', 'template-maker') # todo: change me
 
 class DevConfig(Config):
-    ENV = 'dev'
+    ENV = os.environ.get('TEMPLATE_MAKER_ENV', 'dev')
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://localhost/template_maker')
     DEBUG_TB_INTERCEPT_REDIRECTS = False
     BROWSERID_URL = os.environ.get('BROWSERID_URL', 'http://localhost:9000')
 
 class ProdConfig(Config):
-    ENV = 'prod'
+    ENV = os.environ.get('TEMPLATE_MAKER_ENV', 'prod')
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     BROWSERID_URL = os.environ.get('BROWSERID_URL')
