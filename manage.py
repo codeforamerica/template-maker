c36c4cdc385cc65ff78458e4c529b2678aa99267
bsmithgall <ben.smithgall@gmail.com>
2015-03-22 21:41:34 -0400

    update readme, make shell better

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 import os
-from flask.ext.script import Manager, Server
+from flask.ext.script import Manager, Shell, Server
 from flask.ext.migrate import MigrateCommand
 from flask.ext.assets import ManageAssets
 
-from template_maker.app import create_app
+from template_maker.app import create_app, db
 from template_maker.settings import DevConfig, ProdConfig
 
 if os.environ.get('TEMPLATE_MAKER_ENV') == 'prod':
@@ -19,6 +19,7 @@ def _make_context():
 
 manager = Manager(app)
 manager.add_command('server', Server(port=9000))
+manager.add_command('shell', Shell(make_context=_make_context))
 manager.add_command('db', MigrateCommand)
 manager.add_command('assets', ManageAssets)
 
